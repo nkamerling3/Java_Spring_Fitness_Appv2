d55cecf78416b47f08efa1e75315e131df2f76b3
Noah Kamerling <noahkamerling@gmail.com>
2024-11-22 00:36:33 -0500

    initial commit

---
+++ src/main/java/dev/noahkamerling/runnerz/Application.java
@@ -0,0 +1,18 @@
+package dev.noahkamerling.runnerz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("app started successfully");
+	}
+
+}
---
+++ src/main/java/dev/noahkamerling/runnerz/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package dev.noahkamerling.runnerz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMessage {
+
+    public String getWelcomeMessage() {
+        return "Welcome to the Spring Boot Application";
+    }
+
+}
